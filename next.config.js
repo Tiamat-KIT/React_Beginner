bf14910cc87fe3672e5d385a807cdeb214a01000
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2023-06-30 12:55:48 +0900

    update

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    pageExtensions: ["ts","tsx","js","jsx","md","mdx"]
 }
 
-const withMDX = require("@next/mdx")({
-    extension: /\.mdx?$/,
-    options :{
-        remarkPlugins:[],
-        rehypePlugins: [],
-        providerImportSource: "@mdx-js/react"
-    },
-    
-})
-
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
